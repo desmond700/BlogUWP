42839a4a5da38ce7fd4e81a547446d9059a18eac
desmond wallace <desmond.19@live.co.uk>
2019-02-03 22:11:04 -0500

    modified

--- BlogUWP/Views/ProfilePage.xaml.cs
+++ BlogUWP/Views/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BlogUWP.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +27,28 @@ namespace BlogUWP.Views
         public ProfilePage()
         {
             this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (e.Parameter is User user)
+            {
+                name.Text = user.Firstname+" "+user.Lastname+"("+user.Username+")";
+                if (user.Image != null)
+                {
+                    userimg.Source = new BitmapImage(new Uri("/Assets/" + user.Image));
+                }
+                if (user.Email != null)
+                {
+                    email_txt.Text = user.Email;
+                }
+                if (user.DateOfBirth != null)
+                {
+                    DateTimeOffset dateOfBirth = user.DateOfBirth.Value;
+                    bday_txt.Text = dateOfBirth.Month.ToString() +", "+ dateOfBirth.Day.ToString() + ", " + dateOfBirth.Year.ToString();
+                }
+            }
         }
     }
 }
